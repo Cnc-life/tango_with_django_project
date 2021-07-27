0e2463439bfd40e12cd6b4b6d221d31c3b6b2ef3
Xiaoming Zhang <2585537Z@gla.student.ac.uk>
2021-07-27 14:44:23 +0100

    “chapter3 git push origin master git commit -m “chapter3

--- rango/urls.py
+++ rango/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
-from rango import views
+from django.urls import path
+from rango import views, admin
+
+app_name = 'rango'
 
 urlpatterns = [
-url(r'^$', views.index, name='index'),
-url(r'^$', views.about, name='about'),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
 ]
