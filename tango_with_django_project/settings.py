bcfcce1b10ed12543a1850c74abf4d8ad312c311
Xiaoming Zhang <2585537Z@gla.student.ac.uk>
2021-07-30 05:57:35 +0100

    chapter9

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -19,6 +19,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR = os.path.join(BASE_DIR, 'media')
 
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
@@ -127,3 +128,6 @@ STATICFILES_DIRS = [STATIC_DIR]
 
 MEDIA_ROOT = MEDIA_DIR
 MEDIA_URL = '/media/'
+
+LOGIN_URL = '/rango/login/'
+LOGIN_URL = 'rango:login'
